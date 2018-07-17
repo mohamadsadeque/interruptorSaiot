177a8272224c2742c7fb58fe36deac14c41535d1
ricavalcanti <ricavalcanti@bct.ect.ufrn.br>
2018-07-17 15:37:08 -0300

    v0.5
    Problemas:
    
    falta uma variável/lógica que armazene o estado atual corretamente;
    
    estado do rele mudando corretamente, porém ele para de funcionar se chavear o interruptor muito rapidamente;
    
    função chamada pela interrupção inumeras vezes devido o ruído do interruptor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,32 @@
 #include <Arduino.h>
-int estado = 0;
-int tensao = 0;
-int ultimaTensao = 0;
-int sensorValue = 0;
-const int Rele = 7;
-const int sensorTensao = A0;
-int condicao = 0; 
- 
 
-void interrupcao(){
-  if (estado == HIGH) {    
-   estado = 0;
+volatile int estado = 0;
+const int Rele = D3;
+const int pinInterrupt = D7;
+
+void ICACHE_RAM_ATTR interrupcao(){
+  if(estado^digitalRead(pinInterrupt)){
+    estado = !estado;
+    Serial.println("Mudou estado");  
+    Serial.println(estado);
+    delay(10);
   }
-  else {
-   estado = 1;
-  }
-  delay(20);
 }
+
 void setup() {
  pinMode(Rele, OUTPUT);
- pinMode(sensorTensao, INPUT);
- Serial.begin(9600);
- pinMode(2, INPUT);
- // attachInterrupt(0,interrupcao,RISING); //Configurando a interrupção
+ pinMode(pinInterrupt, INPUT_PULLUP);
+attachInterrupt(digitalPinToInterrupt(pinInterrupt),interrupcao,CHANGE); //Configurando a interrupção
+Serial.begin(115200);
+Serial.println("START");
+delay(50);
 }
 
 void loop(){
-  /*if(digitalRead(2)){
-    if(estado == HIGH){
-      estado = 0;
-      }
-    else{
-      estado = 1;
-      }
-     delay(20);
-    }
-  
-  CondicaoAC();
-  if(tensao != ultimaTensao){
-    if(estado == HIGH){
-      estado = 0;
-      }
-    else{
-      estado = 1;
-      }
-     ultimaTensao = tensao; 
-    }
-digitalWrite(Rele, estado);  */
-Serial.println(analogRead(sensorTensao));
-delay(20);
-
-}
-
-void CondicaoAC(){
-  for (int i = 0; i < 30; i++){ 
-    if(analogRead(sensorTensao) < 70){ 
-   tensao = 0;
-   delay(10);
-    }else{ 
-   tensao = 220;
-   delay(40);      
-    }
-  }  
+digitalWrite(Rele, estado); 
+/*if(digitalRead(pinInterrupt) ){
+  estado = !estado;
+}*/
 }
 
   
