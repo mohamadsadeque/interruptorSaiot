f49e5c3e665fe6f4a2467f25e85cefdb55c72edf
Unknown <ricjose2005@hotmail.com>
2019-03-28 21:40:38 -0300

    WiP
    Lógica para filtro feita apenas para o modo AP, sem utilizar o bloqueio.

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,16 @@ Hardware:
 #include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
 #include <SaIoTDeviceLib.h>
+#include <Ticker.h>
+
 #define CHAVE_1 14
 #define CHAVE_2 12
 
 const int LED = 10;
 const int RELE_1 = D7; // D7
 const int RELE_2 = D8; // D8
+const int tempoCadaAmostragem = 10; //em MS
+const int maxLeituras = 60;
 
 //estados
 volatile bool stateButton = false;
@@ -46,6 +50,12 @@ unsigned long int lastTime_2 = 0;
 unsigned long int delayLeitura_2 = 0;
 short int media_2 = 0;
 short int leituras_2 = 0;
+volatile short int qntLeituras_1 = 0;
+volatile short int qntLeituras_2 = 0;
+
+//interrupçao interna
+Ticker amostragem_1;
+Ticker amostragem_2;
 
 int report(int, int);
 void interrupcao_1();
@@ -73,6 +83,8 @@ void setup();
 void loop();
 //funções
 void writeAndReport(int port, int value);
+void setAmostragem1();
+void setAmostragem2();
 
 void setup()
 {
@@ -81,6 +93,7 @@ void setup()
   pinMode(CHAVE_2, INPUT_PULLUP);
   pinMode(RELE_1, OUTPUT);
   pinMode(RELE_2, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   delay(80);
   attachInterrupt(digitalPinToInterrupt(CHAVE_1), interrupcao_1, FALLING);
@@ -97,44 +110,6 @@ void setup()
 void loop()
 {
   sonoff.handleLoop();
-  if (lendo_1)
-  {
-    calcMedia(CHAVE_1);
-  }
-  if (lendo_2)
-  {
-    calcMedia(CHAVE_2);
-  }
-
-  if (cont != 0 && !contando) //inicio da contagem de vezes apertadas/tempo
-  {
-    countTime = millis();
-    contando = true;
-  }
-  else if (cont >= 8)
-  {
-    //zerar e bloquear
-    digitalWrite(LED, HIGH);
-    bloquear = true;
-    blockTime = millis();
-    Serial.println("BLOQUEOU");
-    cont = 0;
-    contando = false;
-  }
-  else if (contando && (abs(millis() - countTime) > 10000))
-  {
-    //zerar
-    cont = 0;
-    contando = false;
-    Serial.println("Resetou a contagem");
-  }
-  if (bloquear && (abs(millis() - blockTime) > 10000))
-  {
-    Serial.println("Desbloqueou ");
-    bloquear = false;
-    digitalWrite(LED, LOW);
-  }
-
   if (isChanged01)
   {
     cont++;
@@ -149,16 +124,127 @@ void loop()
   }
   if (stateButton)
   {
-    if(stateLED_1 == stateLED_2 && stateLED_1 == 0){
+    if (stateLED_1 == stateLED_2 && stateLED_1 == 0)
+    {
       stateButton = stateLED_1;
       //report
-      report(-1,stateButton);
+      report(-1, stateButton);
     }
-  }else{
-    if(stateLED_1 || stateLED_2){
+  }
+  else
+  {
+    if (stateLED_1 || stateLED_2)
+    {
       stateButton = 1;
-      report(-1,stateButton);
+      report(-1, stateButton);
     }
+  }
+}
+
+void writeAndReport(int port, int value)
+{
+  digitalWrite(port, value);
+  String cKey;
+  report(port, value);
+}
+void setReconfigura()
+{
+  reconfigura = true;
+}
+
+void interrupcao_1()
+{
+  if (!lendo_1 && (abs(millis() - lastTime_1) > 300) && !bloquear) //trocar 300 por um define -> intervalo de tempo entre o inicio de duas leituras
+  {
+    Serial.println("Habilitou01");
+    amostragem_1.attach_ms(tempoCadaAmostragem, setAmostragem1);
+    lendo_1 = true;
+  }
+}
+void interrupcao_2()
+{
+  if (!lendo_2 && (abs(millis() - lastTime_2) > 300) && !bloquear)
+  {
+    Serial.println("Habilitou02");
+    amostragem_2.attach_ms(tempoCadaAmostragem, setAmostragem2);
+    lendo_2 = true;
+  }
+}
+
+void setAmostragem1()
+{
+  if (!digitalRead(CHAVE_1))
+  {
+    media_1++;
+  }
+  qntLeituras_1++;
+  if (media_1 >= (maxLeituras/2) || qntLeituras_1 >= maxLeituras)
+  {
+    if (media_1 >= (maxLeituras/2))
+    {
+      stateLED_1 = !stateLED_1;
+      isChanged01 = true;
+      Serial.println("ACENDER LUZ 1");
+      //acender luz
+    }
+    media_1 = 0;
+    qntLeituras_1 = 0;
+    lendo_1 = false;
+    lastTime_1 = millis();
+    //desabilitar interrupt
+    Serial.println("Desabilitou01");
+    amostragem_1.detach();
+    return;  
+  }
+}
+
+void setAmostragem2()
+{
+  if (!digitalRead(CHAVE_2))
+  {
+    media_2++;
+  }
+  qntLeituras_2++;
+  if (media_2 >= (maxLeituras/2) || qntLeituras_2 >= maxLeituras)
+  {
+    if (media_2 >= (maxLeituras/2))
+    {
+      stateLED_2 = !stateLED_2;
+      isChanged02 = true;
+      Serial.println("ACENDER LUZ 2");
+      digitalWrite(LED_BUILTIN,!stateLED_2);
+      //acender luz
+    }
+    media_2 = 0;
+    qntLeituras_2 = 0;
+    lendo_2 = false;
+    lastTime_2 = millis();
+    Serial.println("Desabilitou02");
+    amostragem_2.detach();
+    //desabilitar interrupt
+    return;
+  }
+}
+
+int report(int type, int value)
+{
+  String cKey;
+  if (type == RELE_1)
+  {
+    cKey = toggle_1.getKey();
+  }
+  else if (type == RELE_2)
+  {
+    cKey = toggle_2.getKey();
+  }
+  else
+  {
+    cKey = onOff.getKey();
+  }
+
+  if (!sonoff.reportController(cKey, value))
+  {
+    Serial.println("Erro ao enviar dados pro SaIoT");
   }
 }
 
@@ -186,8 +272,10 @@ void callback(char *topic, byte *payload, unsigned int length)
       Serial.print("Estado led_1: ");
       Serial.println(stateLED_1);
       //chegando um dado direto pra um dos botoes menores, atualiza o estado e escreve na porta
-    }else{
-      report(RELE_1,stateLED_1);
+    }
+    else
+    {
+      report(RELE_1, stateLED_1);
     }
   }
   if (strcmp(topic, (sonoff.getSerial() + toggle_2.getKey()).c_str()) == 0)
@@ -200,8 +288,10 @@ void callback(char *topic, byte *payload, unsigned int length)
       stateLED_2 = payloadS.toInt();
       Serial.print("Estado led_2: ");
       Serial.println(stateLED_2);
-    }else{
-      report(RELE_2,stateLED_2);
+    }
+    else
+    {
+      report(RELE_2, stateLED_2);
     }
   }
   if (strcmp(topic, (sonoff.getSerial() + onOff.getKey()).c_str()) == 0)
@@ -216,125 +306,10 @@ void callback(char *topic, byte *payload, unsigned int length)
       stateLED_2 = valueStateRecived;
       writeAndReport(RELE_1, valueStateRecived);
       writeAndReport(RELE_2, valueStateRecived);
-    }else{
-      report(-1,stateButton);
     }
-  }
-}
-
-void writeAndReport(int port, int value)
-{
-  digitalWrite(port, value);
-  String cKey;
-  report(port, value);
-}
-void setReconfigura()
-{
-  reconfigura = true;
-}
-
-void calcMedia(int chave)
-{
-  if (chave == CHAVE_1)
-  {
-    if (abs(millis() - delayLeitura_1) > 10) //colocar define pra esse 10 -> intervalo de tempo entre cada leitura
+    else
     {
-      if (leituras_1 < 20) //define pra esse 20 -> qnt de leituras
-      {
-        if (!digitalRead(CHAVE_1))
-        {
-          media_1++;
-        }
-        leituras_1++;
-        if (media_1 >= 10)
-        {
-          leituras_1 = 25;
-        }
-      }
-
-      if (leituras_1 >= 20) //colocar isso em uma função seria bom
-      {
-        if (media_1 >= 10)
-        {
-          stateLED_1 = !stateLED_1;
-          isChanged01 = true;
-        }
-        media_1 = 0;
-        leituras_1 = 0;
-        lendo_1 = false;
-        lastTime_1 = millis();
-      }
-      delayLeitura_1 = millis();
+      report(-1, stateButton);
     }
-  }
-  /////////////////////////////////////////// CHAVE 2
-  else
-  {
-    if (abs(millis() - delayLeitura_2) > 10)
-    {
-      if (leituras_2 < 15)
-      {
-        if (!digitalRead(CHAVE_2))
-        {
-          media_2++;
-        }
-        leituras_2++;
-        if (media_2 >= 10)
-        {
-          leituras_2 = 25;
-        }
-      }
-
-      if (leituras_2 >= 15)
-      {
-        if (media_2 >= 10)
-        {
-          stateLED_2 = !stateLED_2;
-          isChanged02 = true;
-        }
-        media_2 = 0;
-        leituras_2 = 0;
-        lendo_2 = false;
-        lastTime_2 = millis();
-      }
-      delayLeitura_2 = millis();
-    }
-  }
-}
-
-void interrupcao_1()
-{
-  if (!lendo_1 && (abs(millis() - lastTime_1) > 300) && !bloquear) //trocar 300 por um define -> intervalo de tempo entre o inicio de duas leituras
-  {
-    lendo_1 = true;
-  }
-}
-void interrupcao_2()
-{
-  if (!lendo_2 && (abs(millis() - lastTime_2) > 300) && !bloquear)
-  {
-    lendo_2 = true;
-  }
-}
-
-int report(int type, int value)
-{
-  String cKey;
-  if (type == RELE_1)
-  {
-    cKey = toggle_1.getKey();
-  }
-  else if (type == RELE_2)
-  {
-    cKey = toggle_2.getKey();
-  }
-  else
-  {
-    cKey = onOff.getKey();
-  }
-
-  if (!sonoff.reportController(cKey, value))
-  {
-    Serial.println("Erro ao enviar dados pro SaIoT");
   }
 }
